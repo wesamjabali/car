a7f6f87413bc34b93ee3a5aed39ed759db4d5d40
MikeFindlay <55937227+MikeFindlay@users.noreply.github.com>
2020-12-21 19:37:31 -0600

    Create lidar.ino

---
+++ lidar.ino
@@ -0,0 +1,54 @@
+/*
+ * Serial1 = pins 0/1
+ * Pin 23 -> motor control 
+ * 5v PSU 
+ */
+ 
+// This sketch code is based on the RPLIDAR driver library provided by RoboPeak
+#include <RPLidar.h>
+
+// You need to create an driver instance 
+RPLidar lidar;
+
+#define RPLIDAR_MOTOR 23 // The PWM pin for control the speed of RPLIDAR's motor.
+                        // This pin should connected with the RPLIDAR's MOTOCTRL signal 
+                       
+                        
+void setup() {
+  // bind the RPLIDAR driver to the arduino hardware serial
+  Serial.begin(9600);
+  lidar.begin(Serial1);
+  // set pin modes
+  pinMode(RPLIDAR_MOTOR, OUTPUT);
+}
+
+void loop() {
+  if (IS_OK(lidar.waitPoint())) {
+    float distance = lidar.getCurrentPoint().distance; //distance value in mm unit
+    float angle    = lidar.getCurrentPoint().angle; //anglue value in degree
+    bool  startBit = lidar.getCurrentPoint().startBit; //whether this point is belong to a new scan
+    byte  quality  = lidar.getCurrentPoint().quality; //quality of the current measurement
+    if(angle >= 0 && angle <= 10) { 
+      Serial.printf("Angle: %f\n", angle);
+      Serial.printf("Distance: %f mm\n\n", distance);
+      delay(500); 
+    }
+    //perform data processing here... 
+    
+    
+  } else {
+    Serial.write("NOT OK\n");
+    analogWrite(RPLIDAR_MOTOR, 0); //stop the rplidar motor
+    
+    // try to detect RPLIDAR... 
+    rplidar_response_device_info_t info;
+    if (IS_OK(lidar.getDeviceInfo(info, 100))) {
+       // detected...
+       lidar.startScan();
+       
+       // start motor rotating at max allowed speed
+       analogWrite(RPLIDAR_MOTOR, 255);
+       delay(1000);
+    }
+  }
+}
